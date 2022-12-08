8868193d4c725a9faa098cd33586e9f252fd4db4
Emmanuel Rudolf Jocondo <manurudolf@gmail.com>
2022-12-08 14:56:11 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
 /**
  * main - print quote
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
