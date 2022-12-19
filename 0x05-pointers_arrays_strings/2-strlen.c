6c60b4ecbf5f344e5e947563e8f31793c1291076
Emmanuel Rudolf Jocondo <manurudolf@gmail.com>
2022-12-19 12:22:33 -0800

    the report

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * Description: returns the length of a string
+ *
+ * @str: the strong to get the length of
+ *
+ * Return: the length of @str
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
