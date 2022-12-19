0006dabc2f23b0b45db8d63b11ddf4cebbaee442
Emmanuel Rudolf Jocondo <manurudolf@gmail.com>
2022-12-19 12:37:28 -0800

    i do not fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _puts - prints string followed by newline
+ *
+ * Description: from pointer to string
+ *
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		i++;
+		str++;
+	}
+	_putchar('\n');
+}
