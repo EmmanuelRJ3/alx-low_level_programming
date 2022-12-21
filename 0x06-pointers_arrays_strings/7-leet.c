9b1551cf878e7b8f7bae8db96c5f064867afc815
Emmanuel Rudolf Jocondo <manurudolf@gmail.com>
2022-12-21 14:52:00 -0800

    mozart composed his music

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,41 +1,30 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
+ * leet - encode into 1337 speak
  *
- * Description: function that encodes a string into 1337
+ * Description: function encodes into 1337
  *
- * Letters a and A are replaced by 4
- * Letters e and E are replaced by 3
- * Letters o and O are replaced by 0
- * Letters t and T are replaced by 7
- * Letters l and L are replaced by 1
- * @s: pointers to string
+ * @n: input value
  *
- * Return: pointer to s
+ * Return: n value
  */
 
-char *leet(char *s)
+char *leet(char *n)
 {
-	int stringCount, leetCount;
-	char leetLetters[] = "aAeo0tTlL";
-	char leetNums[] = "4433007711";
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	/* scan through string */
-	stringCount = 0;
-	while (s[stringCount] != '\0')
-		/* check whether leetLetter is found*/
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		leetCount = 0;
-		while (leetCount < 10)
+		for (j = 0; j < 10; j++)
 		{
-			if (leetLetters[leetCount] == s[stringCount])
+			if (n[i] == s1[j])
 			{
-				s[stringCount] = leetNums[leetCount];
+				n[i] = s2[j];
 			}
-			leetCount++;
 		}
-		stringCount++;
 	}
-	return (s);
+	return (n);
 }
