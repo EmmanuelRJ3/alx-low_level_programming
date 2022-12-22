2fb6deba7dcc1423d543e49959e9a574e2e70f91
Emmanuel Rudolf Jocondo <manurudolf@gmail.com>
2022-12-22 05:57:56 -0800

    Revert "dreams become reality"
    This reverts commit 062e03949c8217f90f85067a003996484b0e0761.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,4 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - entry point
- *
- * Description: function returns some integer even '0'
- * at the end of the program execution.
- *
- * Return: void
- */
 
 int main(void)
 {
@@ -19,16 +8,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	p[5] = 98;
-	/* ...so that this prints 98\n */
+/*
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ *   - you are not allowed to modify p
+ *   - only one statement
+ *   - you are not allowed to code anything else thant his line of code
+ */
+	*(p + 5) = 98;
+/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
