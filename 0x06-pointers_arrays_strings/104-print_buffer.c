b818851a9b3fe12ae8c9a54973f3fda4747cdf4e
Emmanuel Rudolf Jocondo <manurudolf@gmail.com>
2022-12-22 08:45:42 -0800

    noise is a buffer more effective

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,55 +2,65 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints a buffer 10 bytes at a time
- * starting with the byte position
- * then showing the hex content then displayign printable characters
+ * print_line - prints a buffer line
+ *
+ * Description: function prints a buffer line
+ *
+ * @b: the pointed buffer
+ * @c: the current position in buffer
+ * @size: the max size of the content to print
+ */
+
+void print_line(char *b, int c, int size)
+{
+	int i = 0;
+
+	printf("%08x: ", c);
+	while (i < 10)
+	{
+		if (i + c >= size)
+			printf("  ");
+		else
+			printf("%02x", b[i + c]);
+		if (i % 2 != 0)
+			printf(" ");
+		i++;
+	}
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i + c < size)
+		{
+			if ((b[i + c] < 32 || b[i + c] > 126))
+				printf(".");
+			else
+				printf("%c", b[i + c]);
+		}
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * print_buffer - prints a buffer
  *
  * Description: function prints a buffer
  *
- * @b: the buffer to be printed
- * @size: the number of bytes to be printed from the buffer
+ * @b: the pointed buffer
+ * @size: the size of the content to print
  */
 
 void print_buffer(char *b, int size)
 {
-	int byte, index;
-
-	for (byte = 0; byte < size; byte += 10)
-	{
-		printf("%08x: ", byte);
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf(" ");
-
-			else
-				printf("%02x", *(b + index + byte));
-
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
-
-			else if (*(b + index + byte) >= 31 &&
-					*(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-
-			else
-				printf(".");
-		}
-
-		if (byte >= size)
-			continue;
-
-		printf("\n");
-	}
+	int i = 0;
 
 	if (size <= 0)
 		printf("\n");
+	else
+		while (i < size)
+		{
+			print_line(b, i, size);
+			i += 10;
+		}
 }
