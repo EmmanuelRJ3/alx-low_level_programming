4682f9288734d8fd1bcde2ec3a723fccf4ac3968
Emmanuel Rudolf Jocondo <manurudolf@gmail.com>
2023-01-03 10:00:29 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - searches string for given character
+ *
+ * Description: searches string for given character
+ *
+ * @s: string given
+ * @c: character given
+ *
+ * Return: returns pointer to character in string, or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+		return ('\0');
+}
