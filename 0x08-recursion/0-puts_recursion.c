8479c760e4099cd60baa2802eb145872b9257258
Emmanuel Rudolf Jocondo <manurudolf@gmail.com>
2023-01-05 04:26:40 -0800

    Revert "forgot a once true locked away secret"
    This reverts commit 8143a8b2b2d87e8f0cf2365c32afbd2386c52326.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,7 +13,7 @@
 void _puts_recursion(char *s)
 {
 
-	if (*s != '\0')
+	if (*s !)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
@@ -21,6 +21,6 @@ void _puts_recursion(char *s)
 
 	else
 	
-		_putchar('\n');
+		putchar('\n');
 	
 }
